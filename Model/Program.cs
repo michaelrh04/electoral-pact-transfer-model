f91247853d43a2fde14df7359e6a26e1344f087d
Michael Hargreaves <contact@michaelrh04.co.uk>
2022-08-25 23:59:28 +0100

    Implement data handling
    Also includes new regions and summaries pending further development

--- Model/Program.cs
+++ Model/Program.cs
@@ -20,14 +20,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using Model.Models;
 
 namespace Model
 {
     class Program
     {
+        #region Program foundational logic
+        /// <summary>
+        /// Initialisation code - the main thread - for the Program class.
+        /// </summary>
+        /// <param name="args">Any init arguments (datatype of argument is object).</param>
         static void Main(string[] args)
         {
-
         }
+        #endregion
+        #region Static variables
+        static List<Constituency> data_ListOfConstituencies;
+        static List<Party> data_ListOfParties;
+        #endregion
+
+        #region Data handling
+        static (List<Constituency>, List<Party>) ReadData(string path)
+        {
+            List<Constituency> outputConsts = new List<Constituency>();
+            List<Party> outputParties = new List<Party>();
+            // Many thanks to Alexander Yumashev and editors for this CSV tip-off.
+            // Attribution link: https://stackoverflow.com/a/20523165.
+            using (TextFieldParser csvReader = new TextFieldParser(path) { TextFieldType = FieldType.Delimited, Delimiters = new string[] { "," } })
+            {
+                bool headers = true;
+                while (!csvReader.EndOfData)
+                {
+                    string[] entry = csvReader.ReadFields();
+                    if (headers)
+                    {
+                        for (int i = 3; i < entry.Length; i++)
+                        {
+                            outputParties.Add(new Party
+                            {
+                                Name = entry[i]
+                            });
+                        }
+                        headers = false;
+
+                    }
+                    else
+                    {
+                        Constituency newConstituency = new Constituency
+                        {
+                            Name = entry[0],
+                            Region = entry[1]
+                        };
+                        outputConsts.Add(newConstituency);
+                    }
+                }
+            }
+            return (outputConsts, outputParties);
+        }
+        #endregion
     }
 }
