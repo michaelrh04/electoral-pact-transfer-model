e3a4806d08202b2849972f188b7b06abc42e057e
Michael Hargreaves <contact@michaelrh04.co.uk>
2022-08-25 22:22:25 +0100

    Create first models

---
+++ Model/Models/Party.cs
@@ -0,0 +1,29 @@
+﻿
+/*
+ * COPYRIGHT (C) 2022 MICHAEL HARGREAVES 
+ * ALL RIGHTS RESERVED
+ * contact@michaelrh04.co.uk
+ * 
+ * This file is part of the electoral-pact-transfer-model project. 
+ * It is subject to the license terms in the LICENSE file found in 
+ * the top-level directory of this distribution and at 
+ * https://github.com/michaelrh04/electoral-pact-transfer-model. 
+ * 
+ * No part of lectoral-pact-transfer-model, including this file, 
+ * may be copied, modified, propagated, or distributed except 
+ * according to the terms contained in the LICENSE file.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+    class Party
+    {
+    }
+}
