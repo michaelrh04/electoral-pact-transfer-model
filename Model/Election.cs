c9ccf5c0454c62619111494771ed504acd8e7957
Michael Hargreaves <contact@michaelrh04.co.uk>
2022-08-26 00:20:19 +0100

    Model foundations and data interpretation (#1)
    **Squash merge contained following developments:**
    * Create first models
    * Data-type selection
    * Create constructor
    * Add regions
    * Implement election outcome logic; _is composed of a dynamic dictionary variable returned in format PARTY, SEATS. Requires future confirmation that PARTY is returned as reference, not value._
    * Conversion logic
    * Constructor modification; _model plan has been changed to permit more than one alliance and more than one alliance member - transfer proportions moved from election class to party class, but will require changing per election._
    * Dataset inclusion
    
    **Notice.** The data used in these datasets is taken from the House of Commons Library, taken from a research briefing. Source: https://commonslibrary.parliament.uk/research-briefings/cbp-8647/, used under the Open Parliament Licence v3.0. Further information regarding this data will be included in README.md pending its development.
    
    * Implement data handling; _also includes new regions and summaries pending further development._
    * Include data-reading logic in main thread
    * Fix dataset and variable omissions
    * Fix incorrect code placement
    * Fix copyright notices; fix maintainability issues

---
+++ Model/Election.cs
@@ -0,0 +1,90 @@
+﻿
+/*
+ * COPYRIGHT (C) 2022 MICHAEL HARGREAVES 
+ * ALL RIGHTS RESERVED
+ * contact@michaelrh04.co.uk
+ * 
+ * This file is part of the electoral-pact-transfer-model project. 
+ * It is subject to the license terms in the LICENSE file found in 
+ * the top-level directory of this distribution and at 
+ * https://github.com/michaelrh04/electoral-pact-transfer-model. 
+ * 
+ * No part of electoral-pact-transfer-model, including this file, 
+ * may be copied, modified, propagated, or distributed except 
+ * according to the terms contained in the LICENSE file.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Model.Models;
+
+namespace Model
+{
+    /// <summary>
+    /// <para>Class for the operation of elections.
+    /// Inherits dictionary data structure: note the structure of
+    /// <c>CONSTITUENCY, PARTY_OF_VICTOR.</c></para>
+    /// </summary>
+    class Election : Dictionary<Constituency, Party>
+    {
+        #region Constructor
+        /// <summary>
+        /// Constructor for the Election class. Requires a data parameter. This class represents one election.
+        /// </summary>
+        /// <param name="data">A list of constituencies that forms the base electoral data.</param>
+        /// <param name="alliances">A two-dimensional array of all party alliances.</param>
+        public Election(List<Constituency> data, Party[,] alliances = null)
+        {
+            InputPreparation(data);
+        }
+        #endregion
+
+        #region Public variables
+        /// <summary>
+        /// A value that represents the outcome of this election in a dictionary <c>PARTY, SEATS</c> format.
+        /// </summary>
+        public Dictionary<Party, int> Outcome
+        {
+            get
+            {
+                var outcome = new Dictionary<Party, int>();
+                foreach (KeyValuePair<Constituency, Party> constituencyResult in this)
+                {
+                    try
+                    {
+                        outcome[constituencyResult.Value] += 1;
+                    }
+                    catch (KeyNotFoundException)
+                    {
+                        outcome.Add(constituencyResult.Value, 1);
+                    }
+                }
+                return outcome;
+            }
+        }
+        #endregion Public variables
+
+        #region Data read and model construction
+        /// <summary>
+        /// Prepares the data input for the <c>Run()</c> procedure in conversion to dictionary format.
+        /// </summary>
+        /// <param name="data">A list of constituencies that forms the base electoral data.</param>
+        /// <returns>A dictionary of all constituencies, with party set in all cases to <c>NULL</c>. Returned data must be <c>Run()</c> before useful.</returns>
+        private void InputPreparation(List<Constituency> data)
+        {
+            foreach (Constituency constituency in data)
+            {
+                Add(constituency, null);
+            }
+        }
+        #endregion
+
+        #region Model logic
+        #endregion
+    }
+}
--- Model/Program.cs
+++ Model/Program.cs
@@ -9,7 +9,7 @@
  * the top-level directory of this distribution and at 
  * https://github.com/michaelrh04/electoral-pact-transfer-model. 
  * 
- * No part of lectoral-pact-transfer-model, including this file, 
+ * No part of electoral-pact-transfer-model, including this file, 
  * may be copied, modified, propagated, or distributed except 
  * according to the terms contained in the LICENSE file.
  * 
@@ -20,14 +20,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using Model.Models;
 
 namespace Model
 {
     class Program
     {
+        #region Program foundational logic
+        /// <summary>
+        /// Initialisation code - the main thread - for the Program class.
+        /// </summary>
+        /// <param name="args">Any init arguments (datatype of argument is object).</param>
         static void Main(string[] args)
         {
-
+            var path = /*INSERT PATH OF DATASET*/;
+            Console.WriteLine(path);
+            (data_ListOfConstituencies, data_ListOfParties) = ReadData(path);
         }
+        #endregion
+
+        #region Static variables
+        static List<Constituency> data_ListOfConstituencies;
+        static List<Party> data_ListOfParties;
+        #endregion
+
+        #region Data handling
+        static (List<Constituency>, List<Party>) ReadData(string path)
+        {
+            List<Constituency> outputConsts = new List<Constituency>();
+            List<Party> outputParties = new List<Party>();
+            // Many thanks to Alexander Yumashev and editors for this CSV tip-off.
+            // Attribution link: https://stackoverflow.com/a/20523165.
+            using (TextFieldParser csvReader = new TextFieldParser(path) { TextFieldType = FieldType.Delimited, Delimiters = new string[] { "," } })
+            {
+                bool headers = true;
+                while (!csvReader.EndOfData)
+                {
+                    string[] entry = csvReader.ReadFields();
+                    if (headers)
+                    {
+                        for (int i = 3; i < entry.Length; i++)
+                        {
+                            outputParties.Add(new Party
+                            {
+                                Name = entry[i]
+                            });
+                        }
+                        headers = false;
+
+                    }
+                    else
+                    {
+                        Constituency newConstituency = new Constituency
+                        {
+                            Name = entry[0],
+                            Region = entry[1]
+                        };
+                        outputConsts.Add(newConstituency);
+                    }
+                }
+            }
+            return (outputConsts, outputParties);
+        }
+        #endregion
     }
 }
